d89684c97185f023a3b96ee3869d2157a5a74585
Kiran Anto <kirananto@gmail.com>
2023-02-01 20:53:09 +0530

    fix: change theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,7 @@ const Layout = ({ location, title, children }) => {
     header = (
       <h3
         style={{
-          fontFamily: `Montserrat, sans-serif`,
+          fontFamily: `Space Grotesk, sans-serif`,
           marginTop: 0,
         }}
       >
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,99 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import { MOBILE_MEDIA_QUERY } from "typography-breakpoint-constants"
+import gray from "gray-percentage"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
+const typography = new Typography({
+  title: "Wordpress Theme 2016",
+  baseFontSize: "16px",
+  baseLineHeight: 1.75,
+  scaleRatio: 5 / 2,
+  googleFonts: [
+    {
+      name: "Space Grotesk",
+      styles: ["700"],
     },
-  }
-}
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+    {
+      name: "Space Grotesk",
+      styles: ["400", "400i", "700", "700i", "900", "900i"],
+    },
+  ],
+  headerFontFamily: ["Space Grotesk", "Georgia", "serif"],
+  bodyFontFamily: ["Space Grotesk", "Georgia", "serif"],
+  bodyColor: "hsla(0,0%,0%,0.9)",
+  headerWeight: 900,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideThemeStyles: () => {
+    return {
+      "a.gatsby-resp-image-link": {
+        boxShadow: `none`,
+      },
+    }
+  },
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+    h1: {
+      fontFamily: ["Space Grotesk", "sans-serif"].join(","),
+    },
+    blockquote: {
+      ...scale(1 / 5),
+      color: gray(41),
+      fontStyle: "italic",
+      paddingLeft: rhythm(13 / 16),
+      marginLeft: rhythm(-1),
+      borderLeft: `${rhythm(3 / 16)} solid ${gray(10)}`,
+    },
+    "blockquote > :last-child": {
+      marginBottom: 0,
+    },
+    "blockquote cite": {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: options.bodyColor,
+      fontWeight: options.bodyWeight,
+    },
+    "blockquote cite:before": {
+      content: '"— "',
+    },
+    ul: {
+      listStyle: "disc",
+    },
+    "ul,ol": {
+      marginLeft: 0,
+    },
+    [MOBILE_MEDIA_QUERY]: {
+      "ul,ol": {
+        marginLeft: rhythm(1),
+      },
+      blockquote: {
+        marginLeft: rhythm(-3 / 4),
+        marginRight: 0,
+        paddingLeft: rhythm(9 / 16),
+      },
+    },
+    "h1,h2,h3,h4,h5,h6": {
+      marginTop: rhythm(2),
+    },
+    h4: {
+      letterSpacing: "0.140625em",
+      textTransform: "uppercase",
+    },
+    h6: {
+      fontStyle: "italic",
+    },
+    a: {
+      boxShadow: "0 1px 0 0 currentColor",
+      color: "#007acc",
+      textDecoration: "none",
+    },
+    "a:hover,a:active": {
+      boxShadow: "none",
+    },
+    "mark,ins": {
+      background: "#007acc",
+      color: "white",
+      padding: `${rhythm(1 / 16)} ${rhythm(1 / 8)}`,
+      textDecoration: "none",
+    },
+  })})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
